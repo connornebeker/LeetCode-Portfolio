3cddd4a4d1b890be22fe45960dcd4349f9bf321e
connornebeker <connor.nebeker03@gmail.com>
2026-01-13 15:28:43 -0700

    first problem solved

---
+++ easy_problems/palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(x: int) -> bool:
+        x = str(x)
+        y = x[::-1]
+        if x == y:
+            return True
+        else:
+            return False
+    
+    print(isPalindrome(12321))
